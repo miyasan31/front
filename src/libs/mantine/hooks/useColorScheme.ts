890af7a49c7fe9f0a2db76f9801fa0b0a40f80bd
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-20 23:15:20 +0900

    ✨ mantine hooks

---
+++ src/libs/mantine/hooks/useColorScheme.ts
@@ -0,0 +1,21 @@
+import type { ColorScheme } from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+
+export const useColorScheme = () => {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  };
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  return {
+    colorScheme,
+    toggleColorScheme,
+  };
+};
