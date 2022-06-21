24555aa97abbd14ccfe62836e866b6d534b02d1f
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-22 01:36:22 +0900

    🔥

--- src/routes/public/index.tsx
+++ src/routes/public/index.tsx
@@ -1,14 +1,5 @@
-import { lazy } from "react";
-
 import { PublicLayout } from "~/components/layout/PublicLayout";
 import { FetchProvider } from "~/components/provider/Fetch";
-import { nestPostsRoutes } from "~/routes/public/nest-posts";
-
-const AboutPage = lazy(() => import("~/components/page/public/about.page"));
-const NestOnePage = lazy(() => import("~/components/page/public/nest-one.page"));
-const NestPostsPage = lazy(() => import("~/components/page/public/nest-posts.page"));
-const PostDetailPage = lazy(() => import("~/components/page/public/post-detail.page"));
-const PostListPage = lazy(() => import("~/components/page/public/posts.page"));
 
 export const publicRoutes = [
   {
@@ -18,29 +9,6 @@ export const publicRoutes = [
         <FetchProvider />
       </PublicLayout>
     ),
-    children: [
-      {
-        path: "about",
-        element: <AboutPage />,
-      },
-      {
-        path: "posts",
-        element: <PostListPage />,
-      },
-      {
-        path: "posts/:postId",
-        element: <PostDetailPage />,
-      },
-      {
-        path: "nest-one",
-        element: <NestOnePage />,
-      },
-      {
-        path: "nest-posts",
-        element: <NestPostsPage />,
-        children: [...nestPostsRoutes],
-      },
-    ],
   },
 ];
 
