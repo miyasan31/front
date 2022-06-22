6d120dbaa91be34c31888e06e075a61d5ea74b7a
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-23 01:44:38 +0900

    ✨ supabase config

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line no-undef
-export const apiBaseUrl = __VITE_API_URL__;
+// export const apiBaseUrl = __VITE_API_URL__;
 
-// export const apiBaseUrl = import.meta.env.VITE_API_URL;
+export const apiBaseUrl = import.meta.env.VITE_API_URL;
+export const supabaseDatabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
+export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
