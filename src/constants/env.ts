fca3751784d97eea69d2aeb6c34ef8883cd20ad4
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-23 19:53:16 +0900

    🚀

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,12 +3,12 @@
 
 const isProductionMode = process.env.NODE_ENV === "production";
 
-export const apiBaseUrl = isProductionMode ? process.env.VITE_API_URL : import.meta.env.VITE_API_URL;
+export const apiBaseUrl = `${isProductionMode ? process.env.VITE_API_URL : import.meta.env.VITE_API_URL}`;
 
-export const supabaseDatabaseUrl = isProductionMode
-  ? process.env.VITE_SUPABASE_DATABASE_URL
-  : import.meta.env.VITE_SUPABASE_DATABASE_URL;
+export const supabaseDatabaseUrl = `${
+  isProductionMode ? process.env.VITE_SUPABASE_DATABASE_URL : import.meta.env.VITE_SUPABASE_DATABASE_URL
+}`;
 
-export const supabaseAnonKey = isProductionMode
-  ? process.env.VITE_SUPABASE_ANON_KEY
-  : import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabaseAnonKey = `${
+  isProductionMode ? process.env.VITE_SUPABASE_ANON_KEY : import.meta.env.VITE_SUPABASE_ANON_KEY
+}`;
