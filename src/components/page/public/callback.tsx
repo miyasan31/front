2046b504305846edc12d563bf0811c43cd0d6f17
miyasan <71614432+miyasan31@users.noreply.github.com>
2022-06-23 20:22:22 +0900

    ✅ リダイレクト確認 (#3)
    * ✅ リダイレクト確認
    
    * 🔇

--- src/components/page/public/callback.tsx
+++ src/components/page/public/callback.tsx
@@ -1,11 +1,6 @@
 import { Head } from "~/components/shared/Head";
-import { authService } from "~/services/auth/authService";
-
-const useAuth = authService.useAuth;
 
 const CallbackInPage = () => {
-  useAuth();
-
   return (
     <>
       <Head title="callback in page" description="callback in page" />
