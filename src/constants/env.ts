aadc34f91ffaafbf6d0b562a6febef0076a4ea22
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-23 19:55:33 +0900

    💡 vercel用

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 // export const apiBaseUrl = __VITE_API_URL__;
 
+// Vercelデプロイ用
 const isProductionMode = process.env.NODE_ENV === "production";
 
 export const apiBaseUrl = `${isProductionMode ? process.env.VITE_API_URL : import.meta.env.VITE_API_URL}`;
