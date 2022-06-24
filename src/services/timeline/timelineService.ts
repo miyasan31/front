2111c9d14be1b21e6210e8efd1245845a4de0bee
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-24 22:03:45 +0900

    ♻️ react-query default fetcher

--- src/services/timeline/timelineService.ts
+++ src/services/timeline/timelineService.ts
@@ -1,16 +1,8 @@
-import { useQuery } from "react-query";
-
-import { apiBaseUrl } from "~/constants/env";
 import type { ITimeline, ITimelineService } from "~/interfaces/service/ITimelineService";
 
 export const timelineService: ITimelineService = {
   useGet: (): ITimeline[] | undefined => {
-    const { data } = useQuery<ITimeline[], Error>(
-      ["timeline"],
-      () => fetch(`${apiBaseUrl}/timeline`).then((res) => res.json()),
-      { suspense: true },
-    );
-
-    return data;
+    console.info("useGet timeline");
+    return undefined;
   },
 };
--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -1,18 +1,10 @@
-import { useQuery } from "react-query";
-
-import { apiBaseUrl } from "~/constants/env";
 import type { IUser } from "~/interfaces/model/IUser";
 import type { IUserService } from "~/interfaces/service/IUserService";
 
 export const userService: IUserService = {
   useGet: (userId: string): IUser | undefined => {
-    const { data } = useQuery<IUser, Error>(
-      ["user", { userId }],
-      () => fetch(`${apiBaseUrl}/users/${userId}`).then((res) => res.json()),
-      { suspense: true },
-    );
-
-    return data;
+    console.info("useGet user userId:", userId);
+    return undefined;
   },
   create: async (user: IUser): Promise<void> => {
     console.info(user);
