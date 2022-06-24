f7577396cdae281ded4d8cb5436a83b002e97fbc
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-24 22:04:01 +0900

    ✨ register page

---
+++ src/components/page/public/register.page.tsx
@@ -0,0 +1,13 @@
+import { Head } from "~/components/shared/Head";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <Head title="register page" description="register page" />
+
+      <div>register</div>
+    </>
+  );
+};
+
+export default RegisterPage;
