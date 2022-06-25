7e83a50486ccb243f374004b05d8b4d6353e4fe5
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-26 01:10:22 +0900

    ♻️

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -3,7 +3,7 @@ import { useCreateUserMutation, useGetUserByIdQuery } from "~/libs/graphql/servi
 
 export const userService: IUserService = {
   useGet: (userId) => {
-    const data = useGetUserByIdQuery({ userId }, { suspense: true });
+    const data = useGetUserByIdQuery({ userId });
     return data;
   },
   create: useCreateUserMutation,
