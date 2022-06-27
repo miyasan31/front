17c898eeb48e01919f83ca21b6acab47182cb3f1
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-28 03:07:15 +0900

    🔥 defaultQueryFn

--- src/providers/react-query.tsx
+++ src/providers/react-query.tsx
@@ -1,24 +1,15 @@
 import type { FC, ReactNode } from "react";
-import type { QueryOptions } from "react-query";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-import { apiBaseUrl } from "~/constants/env";
-
 type Props = {
   children: ReactNode;
-};
-
-const defaultQueryFn: QueryOptions["queryFn"] = async ({ queryKey }) => {
-  const { data } = await fetch(`${apiBaseUrl}/${queryKey[0]}`).then((res) => res.json());
-  return data;
 };
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       suspense: true,
-      queryFn: defaultQueryFn,
       useErrorBoundary: true,
       refetchOnWindowFocus: false,
     },
