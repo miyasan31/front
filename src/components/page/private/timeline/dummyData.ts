72a0200861ac39deb928b0f704ef7597351b97c0
miyasan_0301 <miyasan.19990301@gmail.com>
2022-06-30 01:27:12 +0900

    🚧 タイムラインダミーデータ

---
+++ src/components/page/private/timeline/dummyData.ts
@@ -0,0 +1,71 @@
+export const dummyData = [
+  {
+    user: {
+      id: "user1",
+      name: "John Doe",
+      avatar: "https://lh3.googleusercontent.com/a-/AOh14GhvGMWJM_ZUvI8yalKVp19ZsoK_83l6ydHaFCQK=s96-c",
+    },
+    task: {
+      id: "task1",
+      title: "Task 1",
+      outputMemo:
+        "This is a task. アプトプットメモ！！ アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！アプトプットメモ！！",
+      count: {
+        likeCount: 10,
+        commentCount: 5,
+      },
+      isLike: {
+        id: "like1",
+      },
+    },
+    label: [
+      { id: "label1", name: "Label 1", color: "blue" as const },
+      { id: "label2", name: "Label 2", color: "red" as const },
+      { id: "label3", name: "Label 3", color: "orange" as const },
+    ],
+  },
+  {
+    user: {
+      id: "user2",
+      name: "John Doe",
+      avatar: "https://lh3.googleusercontent.com/a-/AOh14GhvGMWJM_ZUvI8yalKVp19ZsoK_83l6ydHaFCQK=s96-c",
+    },
+    task: {
+      id: "task2",
+      title: "Task 2",
+      outputMemo: "This is a task",
+      count: {
+        likeCount: 10,
+        commentCount: 5,
+      },
+      isLike: null,
+    },
+    label: [
+      { id: "label1", name: "Label 1", color: "blue" as const },
+      { id: "label2", name: "Label 2", color: "red" as const },
+      { id: "label3", name: "Label 3", color: "orange" as const },
+    ],
+  },
+  {
+    user: {
+      id: "user1",
+      name: "John Doe",
+      avatar: "https://lh3.googleusercontent.com/a-/AOh14GhvGMWJM_ZUvI8yalKVp19ZsoK_83l6ydHaFCQK=s96-c",
+    },
+    task: {
+      id: "task3",
+      title: "Task 3",
+      outputMemo: "This is a task",
+      count: {
+        likeCount: 10,
+        commentCount: 5,
+      },
+      isLike: null,
+    },
+    label: [
+      { id: "label1", name: "Label 1", color: "blue" as const },
+      { id: "label2", name: "Label 2", color: "red" as const },
+      { id: "label3", name: "Label 3", color: "orange" as const },
+    ],
+  },
+];
