ec2a6cc2bd43bd3208d76617e8369e0a4ad59bdd
miyasan_0301 <miyasan.19990301@gmail.com>
2022-07-03 04:28:53 +0900

    ✨ ラベル一覧

---
+++ src/components/page/private/task-editor/LabelList/dummyData.ts
@@ -0,0 +1,12 @@
+export const dummyData = [
+  { id: "label1", name: "Label 1", color: "blue" as const },
+  { id: "label2", name: "Label 2", color: "red" as const },
+  { id: "label3", name: "Label 3", color: "orange" as const },
+  { id: "label4", name: "Label 4", color: "lime" as const },
+  { id: "label5", name: "Label 5", color: "green" as const },
+  { id: "label6", name: "Label 6", color: "teal" as const },
+  { id: "label7", name: "Label 7", color: "gray" as const },
+  { id: "label8", name: "Label 8", color: "indigo" as const },
+  { id: "label9", name: "Label 9", color: "pink" as const },
+  { id: "label10", name: "Label 10", color: "violet" as const },
+];
--- src/components/page/private/task-editor/LabelList/index.tsx
+++ src/components/page/private/task-editor/LabelList/index.tsx
@@ -1,5 +1,28 @@
+import { Group } from "@mantine/core";
+import { useRecoilValue } from "recoil";
+
+import { Badge } from "~/components/shared/Badge";
+import { session } from "~/libs/recoil/atom/session";
+
+import { dummyData } from "./dummyData";
+
 const LabelList = () => {
-  return <div>LabelList</div>;
+  const sessionInfo = useRecoilValue(session);
+
+  return (
+    <Group spacing="xs">
+      {dummyData.map((label) => (
+        <Badge
+          key={label.id}
+          type="link"
+          to={`/${sessionInfo.user?.id}/label/${label.id}/task`}
+          size="lg"
+          label={label.name}
+          color={label.color}
+        ></Badge>
+      ))}
+    </Group>
+  );
 };
 
 export default LabelList;
